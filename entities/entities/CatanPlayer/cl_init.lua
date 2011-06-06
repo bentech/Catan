c2a776821724cbd9e0dae9a2eda51234d532a723
Frank Sasto <sastoman@gmail.com>
2011-06-05 21:25:56 -0600

    Added player head tracking

--- entities/entities/CatanPlayer/cl_init.lua
+++ entities/entities/CatanPlayer/cl_init.lua
@@ -8,25 +8,10 @@ function LocalCPlayer()
 end
 
 function ENT:Initialize()
-end
-
-
-local function GetPlayerTrace( pl )
 	
-	local aim = gui.ScreenToVector( gui.MouseX(), gui.MouseY() )
-	local intersectPos = intersectRayPlane( GAMEMODE.View.origin, GAMEMODE.View.origin + aim * 2048, GAMEMODE.ViewOrigin, Vector( 0, 0, 1 ) )
-	return intersectPos
+	self:SetRenderBounds( Vector() * -100, Vector() * 100 )
 	
 end
-local Laser = Material( "cable/redlaser" )
+
 function ENT:Draw()
-	
-	render.SetMaterial( Laser )
-	render.DrawBeam( GAMEMODE.View.origin + Vector( 1, 0, 0 ), GAMEMODE.View.origin + LocalPlayer():GetCursorAimVector() * 2048, 5, 0, 0, Color( 255, 255, 255, 255 ) ) 
-	
-	local tracePos = GetPlayerTrace( LocalPlayer() )
-	
-	if( not tracePos ) then return end
-	render.DrawBeam( SkyboxToWorld( LocalPlayer():EyePos() ), tracePos, 5, 0, 0, Color( 255, 255, 255, 255 ) ) 
-	
 end
